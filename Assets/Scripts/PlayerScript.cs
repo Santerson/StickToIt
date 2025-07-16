8015009ef01900aa93d92c1a14f14030e5163048
Santerson <santacap2019@gmail.com>
2025-07-16 14:45:54 -0700

    asdf

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -26,8 +26,8 @@ public class PlayerScript : MonoBehaviour
     [Header("Jumping")]
     [Tooltip("How much should gravity be set to after the player releases the jump button mid-air?")]
     [SerializeField] private float gravityAmplifier = 1.5f;
-    [Tooltip("Owo screw that spelling")]
-    [SerializeField] private float CoyoteTime = 0.2f;
+    [Tooltip("BAD SPELLING LIVES ON EHEHEHEHEHHEHEHEH (seconds)")]
+    [SerializeField] private float KyoteeTime = 0.2f;
 
     [Header("Keybinds")]
     [SerializeField] private KeyCode leftKey = KeyCode.A;
@@ -36,7 +36,7 @@ public class PlayerScript : MonoBehaviour
 
     private Rigidbody2D rb;
     private float ogGrav;
-    private float CoyoteTimeLeft = 0f;
+    private float KyoteeTimeLeft = 0f;
 
     private void Awake()
     {
@@ -103,12 +103,12 @@ public class PlayerScript : MonoBehaviour
         Debug.DrawRay(new Vector2(origin.x + rightRaycastOffset, origin.y), Vector2.down * groundCheckDistance, hit2.collider != null ? Color.green : Color.red);
         if (hit.collider != null || hit2.collider != null)
         {
-            CoyoteTimeLeft = CoyoteTime;
+            KyoteeTimeLeft = KyoteeTime;
             return hit.collider != null || hit2.collider != null;
         }
-        else if (CoyoteTimeLeft > 0)
+        else if (KyoteeTimeLeft > 0)
         {
-            CoyoteTimeLeft -= Time.deltaTime;
+            KyoteeTimeLeft -= Time.deltaTime;
             return true;
         }
         return false;
