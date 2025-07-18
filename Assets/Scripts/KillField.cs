3f3fa098df31d5e0eb7ecdb86f2923dc3016f76f
Santerson <santacap2019@gmail.com>
2025-07-18 15:01:54 -0700

    Update KillField.cs

--- Assets/Scripts/KillField.cs
+++ Assets/Scripts/KillField.cs
@@ -6,11 +6,22 @@ public class KillField : MonoBehaviour
 {
     [SerializeField] string playerTag = "Player";
     [SerializeField] Vector2 respawnLocation = new Vector2(-7.833f, -0.01f);
+    [SerializeField] bool ResetsLevel = true;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag(playerTag))
         {
-            collision.gameObject.transform.position = respawnLocation;
+            if (ResetsLevel)
+            {
+                // Reset the level by reloading the current scene
+                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+                return;
+            }
+            else
+            {
+                collision.gameObject.transform.position = respawnLocation;
+
+            }
         }
     }
 }
