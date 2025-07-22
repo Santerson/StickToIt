fba712607ad1629b763fd830d5f79e5df926db7d
Aiden Callahan <ajcallahan225@gmail.com>
2025-07-22 13:56:40 -0500

    owo

--- Assets/Scripts/TrollDoor.cs
+++ Assets/Scripts/TrollDoor.cs
@@ -1,13 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TrollDoor : MonoBehaviour
+public class TrollKey : MonoBehaviour
 {
-    [SerializeField] GameObject doorPrefab;
+    [SerializeField] private GameObject exitDoor;       // Drag your "Exit with Door" GameObject here
+    [SerializeField] private Transform exitSpawnPoint;  // Place this where you want the door to appear
 
-    void Start()
+    private bool doorMoved = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Instantiate(doorPrefab, new Vector3(10f,10f,10f) , Quaternion.identity);
+        if (!doorMoved && other.CompareTag("Player"))
+        {
+            // Troll move: door appears to block them
+            exitDoor.transform.position = exitSpawnPoint.position;
+
+            // Optional: if the door had a collider disabled, enable it here
+            // exitDoor.GetComponent<Collider2D>().enabled = true;
+
+            doorMoved = true;
+            Destroy(gameObject); // Remove the key from the scene
+        }
     }
 }
